517e3efb483f94ae101a00d58d2cefb4d333130e
hiro-eng <69486574+hiro-eng@users.noreply.github.com>
2020-11-16 10:03:02 +0900

    Create script3.js

---
+++ script3.js
@@ -0,0 +1,67 @@
+str = "Teck-Teck World";
+document.write("<hr>");
+document.write(str.fontsize(32).italics());
+document.write("<hr>");
+document.write("あなたのお気に入りのwebページを作ります。<br>".bold().italics());
+document.writeln("<img src = 'https://file2-d-cdn.svk.jp//thumbs/20201110-2240_260a3efb198a5af13819bb04f119ccf0.jpg' width= '500px' height ='350px'　alt ='スマホを見る少女'>");
+document.write("<br>>>>>>>このページはJavascript/Jqueryのみで作成してあります。<br>".bold().italics());
+document.write("<br><hr>");
+document.write("HP作成/管理<br>".fontsize(16).italics());
+document.write("企業ロゴ作成/SNS対策/SEO対策<br>".bold().italics());
+document.write("お気軽にお問い合わせください。<br>".bold().italics());
+document.write("Mail: walkerjpjp@gmail.com".bold().italics());
+document.write("<br><hr>");
+document.write("Teck-Teck Worldについて<br>".fontsize(16).italics());
+document.write("埼玉県を拠点として、HIROHIROを中心に活動しています。<br>".bold().italics());
+document.write("インターネット関係のお仕事なら、<br>".bold().italics());
+document.write("個人のお客様、会社様問わずお引き受け致します。".bold().italics());
+document.write("<br><hr>");
+document.write("HIROHIROについて<br>".fontsize(16).italics());
+document.write("さいたま市東大宮在住/一人暮らし/プログラマ/<br>".bold().italics());
+document.write("HTML/CSS/Javascript/Jquery/PHP/Ruby/<br>".bold().italics());
+document.write("食生活アドバイザー保持/簿記、会計、株式投資の知識あり".bold().italics());
+document.write("<br><hr>");
+document.write("このページで使用している画像は「ぱくたそ」のフリー画像です。<br><hr>".bold().italics());
+document.write("「Teck-Teck World」ロゴ<br>".bold().italics())
+document.write("<br><img src = 'https://file19-d-cdn.eay.jp//thumbs/20201110-2158_1c68e0735d0a2c156c6ce1ed12204316.jpg' width = '25%' height = '30%'>");
+document.write("<br><hr>");
+document.write("画面を二回クリックするとメッセージが表示されるよ！<br>".bold().italics());
+
+window.onload = function(){
+   setInterval(function(){
+     var dd = new Date();
+      document.getElementById("T1").innerHTML = dd.toLocaleString();
+    },1000);
+}
+function msg1(){
+  alert("いらっしゃいませ。ご来場ありがとうございます！　by HIROHIRO ");
+
+}
+
+document.bgColor="pink";
+
+　　$(document).ready(function(){
+  $("h1").hover(
+    function(){
+    $("h1").css("color","#ff6666");
+  },
+   function(){
+     $("h1").css("color","black");
+   });
+  $("h1").click(function(){
+
+    $("h1").slideUp();
+  });
+
+ $("#T1").hover(
+   function(){
+    $("#T1").css("color","#ff6666");
+  },
+  function(){
+    $("#T1").css("color","black");
+
+  });
+
+
+
+});
